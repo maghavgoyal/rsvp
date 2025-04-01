2451c0a2149376085c07616be40e5db0ef2b05f8
Maghav Goyal <maghav.goyal@shopee.com>
2025-04-01 17:40:43 +0530

    increased user count to 5

--- src/components/simple-rsvp-form.tsx
+++ src/components/simple-rsvp-form.tsx
@@ -129,6 +129,7 @@ export default function SimpleRsvpForm({ onSubmitSuccess, userId }: SimpleRsvpFo
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
+                <option value="5">5</option>
               </select>
             </div>
 
